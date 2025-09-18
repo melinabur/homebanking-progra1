4c05a932ace6fa6f1d8726fcc2f8572f3c06aa88
Aye Hassan <ayehassan@uade.edu.ar>
2025-09-17 23:55:01 -0300

    Se arregló la condición, para que la opción de Registrarse no apareciera nuevamente cuando el usuario ya está registrado.

--- main.py
+++ main.py
@@ -1,40 +1,60 @@
-#Menu Principal
-
 from usuarios import alta_Usuario, iniciar_sesion
 
+def menu_inicial():
+    usuario_registrado = False   
 
-def menu_inicial ():
     while True:
         print("\n=== Bienvenida/o al Home Banking ===")
-        print("1) Registrarse")
-        print("2) Iniciar sesión")
-        print("0) Cancelar")
 
-        opcion = input("Opción: ")
+        if not usuario_registrado:   
+            print("1) Registrarse")
+            print("2) Iniciar sesión")
+            print("0) Cancelar")
 
-        if opcion == "1":
-            alta_Usuario()
+            opcion = input("Opción: ")
 
-        elif opcion == "2":
-            usuario = iniciar_sesion()
-            
-            if usuario == "SALIR":
+            if opcion == "1":
+                alta_Usuario()
+                usuario_registrado = True   
+
+            elif opcion == "2":
+                usuario = iniciar_sesion()
+                if usuario == "SALIR":
+                    return "SALIR"
+                if usuario is not None:
+                    print("\nBienvenido al sistema,", usuario["nombre"], usuario["apellido"])
+
+            elif opcion == "0":
+                print("Hasta pronto.")
                 return "SALIR"
 
-            if usuario is not None:
-                print("\nBienvenido al sistema,", usuario["nombre"], usuario["apellido"])
-            
-        elif opcion == "0":
-            print ("Hasta pronto.")
-            return "SALIR"
+            else:
+                print("Opción no válida. Por favor, intente nuevamente.")
 
-        else:
-            print("Opción no válida. Por favor, intente nuevamente.")
+        else:   
+            print("1) Iniciar sesión")
+            print("2) Cancelar")
+
+            opcion = input("Opción: ")
+
+            if opcion == "1":
+                usuario = iniciar_sesion()
+                if usuario == "SALIR":
+                    return "SALIR"
+                if usuario is not None:
+                    print("\nBienvenido al sistema,", usuario["nombre"], usuario["apellido"])
+
+            elif opcion == "2":
+                print("Hasta pronto.")
+                return "SALIR"
+
+            else:
+                print("Opción no válida. Por favor, intente nuevamente.")
 
 
-#main principal (invocamos todas las funciones)
+# main principal
 def main():
-    usario = menu_inicial()
+    menu_inicial()
 
 if __name__ == "__main__":
     main()
